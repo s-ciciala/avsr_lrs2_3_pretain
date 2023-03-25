ae8c7cd422fb5f87f5927f76556023f819d8e544
s-ciciala <s1834237@ed.ac.uk>
2023-03-25 20:15:29 +0000

    config change

--- audio_only/preprocessing.py
+++ audio_only/preprocessing.py
@@ -262,7 +262,11 @@ def generate_extended_test_file():
     ##CUT EXAMPLES:
     print("Length of examples before the cull: " + str(len(dirs_test)))
     print("Length of examples before the cull: " + str(len(pretrain_dir)))
-    dirs = test_dir + pretrain_dir[:args["TEST_SIZE"]]
+    dirs = test_dir + dirs_pretrain[:args["TEST_SIZE"]]
+    print("TEST_DIRS")
+    print(dirs_test)
+    print("pretrain_dir")
+    print(dirs_pretrain)
     print("Length of examples after the cull: " + str(len(dirs)))
 
     print("\nFor each folder we will extract the number of txt examples")
