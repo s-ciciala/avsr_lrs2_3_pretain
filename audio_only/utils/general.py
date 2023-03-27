77e74e926e9f76072bdd2fa5d0fe61432b5ba3af
s-ciciala <s1834237@ed.ac.uk>
2023-03-27 18:06:13 +0100

    config change

--- audio_only/utils/general.py
+++ audio_only/utils/general.py
@@ -149,7 +149,7 @@ def evaluate(model, evalLoader, loss_function, device, evalParams):
                 print("------------------TARGET------------------")
                 print("------------------TARGET------------------")
                 print(targetStrings[i])
-            with open('test_results_audio_only.txt', 'w') as f:
+            with open(args["AUDIO_RESULTS_TEXT_FILENAME"], 'w') as f:
                 for i in range(len(predictionStrings)):
                     f.write("------------------TARGET------------------\n")
                     f.write("%s\n" % str(targetStrings[i]))
